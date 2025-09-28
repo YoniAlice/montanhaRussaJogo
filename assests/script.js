9501a7e01e136ca41613b39f003326329e48f555
Marcus Vinicius <marcus.alberes@gmail.com>
2025-09-28 00:09:07 -0300

    Trilho Andando

--- assests/script.js
+++ assests/script.js
@@ -6,8 +6,9 @@ const restart = document.querySelector(".restart");
 //GAME START
 
 function StartGame(){
-
+    perdido.classList.remove('perdeuMesmo');
     obstaculo.classList.add('aniO'); //ANIMAÇÃO DO OBJETO
+    setInterval(moverTrilho,0.2);
 };
     
     const perdeuJogo = setInterval(function(){
@@ -26,7 +27,7 @@ document.addEventListener('click', (e) => {
 
     let el = e.target;
     if(el.contains(restart)) { //RESTART
-        perdido.classList.remove('perdeuMesmo');
+        
         StartGame();
     }
 })
@@ -48,3 +49,40 @@ if (e.key == " " ) { //Tecla espaço
 }
 
 document.addEventListener("keydown", control); //Verifica tecla
+
+//MOVIMENTO DO CENÁRIO
+var trilhos = document.querySelector(".trilhos")
+var trilhoUm = document.querySelector(".trilho");
+var trilhoDois = trilhos.querySelector(':scope > :nth-child(2)');
+let velocidadeDoTrilhoUm = 0.1
+let velocidadeDoTrilhoDois = 99;
+
+function moverTrilho(){ //MOVE O TRILHO PRA ESQUERDA
+
+    //Velocidade dos Trilhos
+
+    velocidadeDoTrilhoUm += -0.13; 
+    trilhoUm.style.left = velocidadeDoTrilhoUm +"%"; 
+
+    velocidadeDoTrilhoDois += -0.13;
+    trilhoDois.style.left = velocidadeDoTrilhoDois +"%";
+
+    // Remove e Adiciona o primeiro Trilho
+    
+    if(velocidadeDoTrilhoUm <= -101){ 
+        trilhos.removeChild(trilhoUm)
+        velocidadeDoTrilhoUm = 99;
+        trilhos.appendChild(trilhoUm)
+    }
+
+    // Remove e Adiciona o segundo Trilho
+    
+        if(velocidadeDoTrilhoDois <= -101){ 
+        trilhos.removeChild(trilhoDois)
+        velocidadeDoTrilhoDois = 99;
+        trilhos.appendChild(trilhoDois)
+    }
+
+}
+
+
